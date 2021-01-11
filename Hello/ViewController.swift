ae6f581de72ee39f9efed4d314109e229ca632f7
Minh Thai Pham <minhthai.pham@foody.vn>
2021-01-11 20:27:12 +0700

    Initial Commit

---
+++ Hello/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hello
+//
+//  Created by Minh Thai on 1/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
