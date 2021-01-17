f3798babfca0839a7f14579bde65fd6fda2426e0
Minh Thai <minhthai40@gmail.com>
2021-01-17 14:51:23 +0700

    add more interactions

--- Hello/ViewController.swift
+++ Hello/ViewController.swift
@@ -8,22 +8,55 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+  @IBOutlet var toggle: UISwitch!
+  @IBOutlet var slider: UISlider!
+  @IBOutlet var button: UIButton!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+    button.addTarget(self, action: #selector(buttonTapped(_:)), for: .touchUpInside)
+  }
+  
+  @IBAction func buttonTapped(_ sender: UIButton) {
+    print("button was tapped!")
+    
+    if toggle.isOn {
+      print("toggle is on")
+    } else {
+      print("toggle is off")
     }
     
-    @IBAction func buttonTapped(_ sender: UIButton) {
-        print("button was tapped!")
+    print("slider value: \(slider.value)")
+  }
+  
+  @IBAction func switchToggled(_ sender: UISwitch) {
+    if sender.isOn {
+      print("switch is on")
+    } else {
+      print("switch is off")
     }
-    
-    @IBAction func switchToggled(_ sender: UISwitch) {
-        if sender.isOn {
-            print("switch is on")
-        } else {
-            print("switch is off")
-        }
+  }
+  
+  @IBAction func sliderValueChanged(_ sender: UISlider) {
+    print(sender.value)
+  }
+  
+  @IBAction func keyboardReturnKeyTapped(_ sender: UITextField) {
+    if let text = sender.text {
+      print(text)
     }
+  }
+  
+  @IBAction func textChanged(_ sender: UITextField) {
+    if let text = sender.text {
+      print(text)
+    }
+  }
+  
+  @IBAction func respondToTapGesture(_ sender: UITapGestureRecognizer) {
+    let location = sender.location(in: view)
+    print(location)
+  }
 }
 
